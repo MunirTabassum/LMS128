cde1be657c6d63d7f4351c7faf21531ed658baa6
Munir Tabassum <munirtabassum2@gmail.com>
2022-07-14 14:30:04 -0700

    barrel file

---
+++ lib/ui/admins_process_flow/admin_screens.dart
@@ -0,0 +1,6 @@
+export 'admins_home.dart';
+export 'add_employ.dart';
+export 'add_trainer.dart';
+export 'profile_screens/view_profiles.dart';
+export 'progress_screens/view_progress.dart';
+export 'delete_employ.dart';
