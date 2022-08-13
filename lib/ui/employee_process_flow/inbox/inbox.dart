5025241a1a33216f4b91ade1180c12fa243d8e97
Munir Tabassum <munirtabassum2@gmail.com>
2022-08-13 11:40:45 -0700

    inbox commit

---
+++ lib/ui/employee_process_flow/inbox/inbox.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:zameer_project/ui/employee_process_flow/my_courses/any_particular_course/message_trainer/message_trainer.dart';
+
+class Inbox extends StatelessWidget {
+  const Inbox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MessageTrainer();
+  }
+}
